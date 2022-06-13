bea75ad704308a1a4110f1782388c3a0ff2a0774
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-06-13 17:48:43 +0530

    done

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+
+const Welcome = (temp) => {
+  return (
+    <>
+      <h1>Hey{temp.name} !</h1>
+      <h2>Welcome to Newton School.</h2>
+    </>
+  );
+};
+
+export default Welcome;
